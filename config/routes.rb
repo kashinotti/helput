101a9458b1708c6e96d06a7047e36c716f3695cf
kashinotti <ryo.ka.notti@gmail.com>
2020-12-12 15:48:42 +0000

    [add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: :users do
+   devise_for :users, controllers: {
+     sessions: 'users/sessions',
+     passwords: 'users/passwords',
+     registrations: 'users/registrations'
+   }
+   root to: 'homes#top'
+   get '/homes/about' => 'homes#about', as: 'homes_about'
+   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+   get '/posts/confirm' => 'posts#confirm', as: 'posts_confirm'
+  end
+
+
+  scope module: :admins do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  end
 end
