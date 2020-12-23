3e00a3f02dc6799774e6886383f8e1f92ded7f8c
kashinotti <ryo.ka.notti@gmail.com>
2020-12-23 10:29:18 +0000

    [update] チャット機能非同期通信

--- app/controllers/users/chats_controller.rb
+++ app/controllers/users/chats_controller.rb
@@ -5,10 +5,13 @@ class Users::ChatsController < ApplicationController
       # form_withで送られてきたパラメータが入っているかを条件分岐で確認
       if UserRoom.where(user_id: params[:chat][:user_id], room_id: params[:chat][:room_id]).present?
         @chat = Chat.create(params.require(:chat).permit(:room_id, :user_id, :message).merge(user_id: current_user.id))
+        respond_to do |format|
+          format.html { redirect_to "room_path(params[:chat][:room_id])" }
+          format.json { render json: @chat }
+        end
       else
         flash[:alert] = "メッセージ送信に失敗しました。"
       end
-      redirect_to room_path(params[:chat][:room_id])
     end
 
 end
