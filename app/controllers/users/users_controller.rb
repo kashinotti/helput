9a5426bc394b5723aef32197809f155cc4462b20
kashinotti <ryo.ka.notti@gmail.com>
2021-01-08 15:10:56 +0000

    [add] プロフィール編集画面　レイアウト

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -4,9 +4,9 @@ class Users::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # 投稿一覧・タイムライン(自分とフォローユーザーの投稿を表示)いいねした投稿を表示するための変数を定義
-    @posts = @user.posts
-    @likes = Like.where(user_id: @user.id)
-    @timelines = Post.where(user_id: [@user.id, @user.followings.ids].flatten).order(created_at: :desc)
+    @posts = @user.posts.page(params[:page]).per(10)
+    @likes = Like.where(user_id: @user.id).page(params[:page]).per(10)
+    @timelines = Post.where(user_id: [@user.id, @user.followings.ids].flatten).order(created_at: :desc).page(params[:page]).per(10)
     # 検索フォームの値を格納するために@qを定義
     @q = Post.ransack(params[:q])
     
