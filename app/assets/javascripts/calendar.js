c085a0f05f82728b675832a827326ffa83d55c2e
kashinotti <ryo.ka.notti@gmail.com>
2021-01-12 13:42:44 +0000

    [modify] 予定表イベント時間がutc表記になる現象。

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -19,30 +19,20 @@ $(document).on('turbolinks:load',function(){
   $('#calendar').fullCalendar({
     events: `/users/${user_id}/events/index.json`,
     titleFormat: 'YYYY年M月',
-
-    // initialView: 'dayGridMonth',
-    // initialView: 'timeGridWeek',
-
-    // initialView: 'timeGridDay',
     selectable: true,
-
     dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
-
     headerToolbar: {
       left: 'month, week, day',
       center: 'title',
       right: 'today, prev, next',
     },
-
+    
+    
     buttonText: {
-      // dayGridMonth: '月',
-      // timeGridWeek: '週',
-      // timeGridDay: '日',
       today: '今日',
       prev: '前月',
       next: '翌月'
     },
-
   　eventColor: '#136FFF',
 
   　eventTextColor: 'white',
--- app/views/users/events/index.json.jbuilder
+++ app/views/users/events/index.json.jbuilder
@@ -1,7 +1,7 @@
 json.array!(@events) do |event|
 
   json.extract! event, :id, :title, :content
-  json.start event.start
-  json.end event.end
+  json.start Time.zone.parse(event.start).since(9.hour).strftime("%F %T")
+  json.end Time.zone.parse(event.end).since(9.hour).strftime("%F %T")
   json.url event_path(event)
 end
