014f51316390b6d33d2f0dc140ec20761a16c3d5
kashinotti <ryo.ka.notti@gmail.com>
2021-01-12 11:09:27 +0000

    [add] 管理側　ログイン画面　レイアウト

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
-layout 'admins'
 class Admins::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  layout 'admins'
   # GET /resource/sign_in
   # def new
   #   super
