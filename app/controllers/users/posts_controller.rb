5639c9478d9e1eadf41603b048bee2dff1668922
kashinotti <ryo.ka.notti@gmail.com>
2021-01-16 12:30:15 +0000

    [add] お問い合わせフォーム画面バリデーション お問い合わせ完了画面ログイン時、未ログイン時ボタン変更

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -17,7 +17,7 @@ class Users::PostsController < ApplicationController
     @user = User.find(current_user.id)
     # ransackの投稿内容の検索結果を表示するための変数を定義
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(10)
+    @posts = @q.result(distinct: true).where(user_id: @user).order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
