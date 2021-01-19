1cea5be5568bde96c84911c28f40ea8bd07400fa
kashinotti <ryo.ka.notti@gmail.com>
2021-01-19 10:12:18 +0000

    [delete]不要なコメントアウト

--- app/controllers/users/chats_controller.rb
+++ app/controllers/users/chats_controller.rb
@@ -4,6 +4,7 @@ class Users::ChatsController < ApplicationController
     def create
       # form_withで送られてきたパラメータが入っているかを条件分岐で確認
       if UserRoom.where(user_id: params[:chat][:user_id], room_id: params[:chat][:room_id]).present?
+        # trueであればuseer_idをmergeしてメッセージを作成する
         @chat = Chat.create(params.require(:chat).permit(:room_id, :user_id, :message).merge(user_id: current_user.id))
         respond_to do |format|
           format.html { redirect_to "room_path(params[:chat][:room_id])" }
